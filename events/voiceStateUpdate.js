0c2ae891f284cc4b72c532872addc7c33b6eff8f
RootAndroid58 <rootand58@gmail.com>
2019-11-27 17:10:44 +0530

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,13 +1,14 @@
 module.exports =  async (oldState, newState) => {
-	//   if (!client.provider.isReady) return;
-	//   const { queue } = client;
-	//   const serverQueue = queue.get(newState.guild.id);
-	//   if (!serverQueue) return;
-	//   if (!oldState.channel) return;
 
-	//   if (oldState.channel.members.size === 1) {
-	//   serverQueue.songs = [];
-    //   await serverQueue.connection.dispatcher.end();
-	//   }
+	//   if (!oldState.channel) return;
+	// if(oldState != newState)
+	// {
+	// 	// moved from one channel to another
+	// }
+	// if(newState == undefined)
+	// {
+	// 	// left the channel 
+	// }
+
   }
 
