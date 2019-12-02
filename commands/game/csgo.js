5c2626cbabe65144ef0e03c79c0b22a83e3ac0eb
RootAndroid58 <rootand58@gmail.com>
2019-12-03 00:17:48 +0530

    Add files via upload

---
+++ commands/game/csgo.js
@@ -0,0 +1,84 @@
+const Discord = require('discord.js');
+var request = require('request');
+var cheerio = require('cheerio');
+const { stripIndents } = require("common-tags");
+
+function getStatData(location , $){
+  
+    var selector = $('.stats-stat .value').eq(location).text();
+
+    var stat_array = $.parseHTML(selector);
+
+    var stat = 0;
+
+    if(stat_array == null || stat_array.lengh == 0){
+        return -1;
+
+    }else{
+        stat = stat_array[0].data;
+    }
+
+    return stat;
+}
+
+module.exports = {
+    name: "csgo",
+    description: "Displays a user's csgo stats!",
+    usage: "csgo <steamid or custom url>",
+    category: "game",
+    aliases: ["cs-go"] , 
+    run: async (client, message, args) => {
+        var UR_L = "http://csgo.tracker.network/profile/" + args[0];
+        console.log(`step 1`)
+
+        if(!args[0]){
+            return message.channel.send("Please Enter a valid STEAMID64 or custom url");
+        }
+        console.log(`step 2`)
+    
+        request(UR_L, function(err, resp, body){
+            console.log(`step 3`)
+    
+            $ = cheerio.load(body);
+    
+            var KD = getStatData(0, $);
+            if(KD == -1){
+                return message.channel.send("Invalid, make sure your profile is not private and you have entered a valid STEAMID64 or Custom URL!");
+            }
+            console.log(`step 4`)
+    
+            var WIN = getStatData(1, $);
+            var HS = getStatData(4, $);
+            var MONEY = getStatData(5, $);
+            var SCORE = getStatData(6, $);
+            var KILLS = getStatData(7, $);
+            var DEATHS = getStatData(8, $);
+            var MVP = getStatData(9, $);
+            var BS = getStatData(13, $);
+            var BD = getStatData(14, $);
+            var HR = getStatData(15, $);
+            console.log(`step 5`)
+    
+            var embed = new Discord.RichEmbed()
+                .setTitle("__***CSGO Stats***__")
+                .setURL(UR_L)
+                .addField("Current stats",stripIndents`
+                Total KD:               __**${KD}**__ 
+                Total Win%:             __**${WIN}**__ 
+                Total MVPs:             __**${MVP}**__
+                Total Score:            __**${SCORE}**__ 
+                Total Kills:            __**${KILLS}**__
+                Total Deaths:           __** ${DEATHS}**__
+                Total Bombs Set:        __** ${BS}**__
+                Total Bombs Defused:    __** ${BD}**__
+                Total Headshots:        __**${HS}**__
+                Total Money Earned:     __**${MONEY}**__
+                Total Hostages Rescued: __**${HR}**__`, true)
+                .addField("Powered by:", `**[csgo.tracker.network](http://csgo.tracker.network)**`, false)
+                .setTimestamp()
+                .setColor("0x#FF0000");
+            message.channel.send(embed);
+            console.log(`step 6`)
+        }
+    )}
+}
